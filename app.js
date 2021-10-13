607eb38fde9644c9ddf11143274605a1ff9738e9
dmundr2 <dmundr2@uic.edu>
2021-10-13 16:55:02 -0500

    Added login route and view

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// my routers
+var loginRouter = require('./routes/login');
+
 var app = express();
 
 // view engine setup
@@ -19,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', loginRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
