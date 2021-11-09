8ab76ad2a5a895ead85ee0cf3b1668a0375b1b14
Sean Kim <seank4@uic.edu>
2021-11-09 17:35:00 -0600

    refactor: format login component

--- New_Project/client/src/components/login.component.js
+++ New_Project/client/src/components/login.component.js
@@ -1,89 +1,106 @@
 import React, { Component } from "react";
-import Axios from 'axios';
-import {Redirect} from 'react-router-dom'
+import Axios from "axios";
+import { Redirect } from "react-router-dom";
 
 export default class Login extends Component {
-
-    constructor(props){
-        super(props);
-        Axios.defaults.withCredentials = true;
-        this.state = {
-            emailLogin: "",
-            passwordLogin: "",
-            loginStatus: "",
-            data: null,
-            loggedIn: false,
-            errorMsg: null
-        };
-    }
-
-    login(pState) {
-        Axios.post('http://localhost:3001/login', {
-            email: pState.emailLogin, 
-            password: pState.passwordLogin})
-            .then((response) =>{
-                if(response.data.message !== 'No User Exists'){
-                    this.setState({loggedIn: true});
-                }
-                else{
-                    this.setState({errorMsg: "Invalid email or password"});
-                }
-            }
-
-        );
+  constructor(props) {
+    super(props);
+    Axios.defaults.withCredentials = true;
+    this.state = {
+      emailLogin: "",
+      passwordLogin: "",
+      loginStatus: "",
+      data: null,
+      loggedIn: false,
+      errorMsg: null,
     };
-    
-    getUser = () => {
-        //console.log("GetUser");
-        Axios.get('http://localhost:3001/user')
-        .then((response) =>{
-            if(response.data.message !== "No authenticated User"){
-                this.setState({loggedIn: true});
-            
-            }
-            else{
-                this.setState({loggedIn: false});
+  }
 
-            }
-        })
-    };
+  login(pState) {
+    Axios.post("http://localhost:3001/login", {
+      email: pState.emailLogin,
+      password: pState.passwordLogin,
+    }).then((response) => {
+      if (response.data.message !== "No User Exists") {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ errorMsg: "Invalid email or password" });
+      }
+    });
+  }
 
-    componentDidMount(){
-        this.getUser();
+  getUser = () => {
+    //console.log("GetUser");
+    Axios.get("http://localhost:3001/user").then((response) => {
+      if (response.data.message !== "No authenticated User") {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  };
+
+  componentDidMount() {
+    this.getUser();
+  }
+
+  render() {
+    if (this.state.loggedIn === true) {
+      return <Redirect to="/dashboard" />;
     }
 
-    render() {
-        if(this.state.loggedIn == true){
-            return <Redirect to ='/dashboard'/>
-        }
+    return (
+      <div>
+        <h3>Sign In</h3>
+        {this.state.errorMsg ? <div>{this.state.errorMsg}</div> : <div></div>}
+        <div className="form-group">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+            onChange={(e) => {
+              this.setState({ emailLogin: e.target.value });
+            }}
+          />
+        </div>
 
-        return (
-            <div>
-                <h3>Sign In</h3>
-                {this.state.errorMsg ? <div>{this.state.errorMsg}</div> : <div></div>}
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" onChange={(e) => {this.setState({emailLogin: e.target.value})}}/>
-                </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+            onChange={(e) => {
+              this.setState({ passwordLogin: e.target.value });
+            }}
+          />
+        </div>
 
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" onChange={(e) => {this.setState({passwordLogin: e.target.value})}}/>
-                </div>
+        <div className="form-group">
+          <div className="custom-control custom-checkbox">
+            <input
+              type="checkbox"
+              className="custom-control-input"
+              id="customCheck1"
+            />
+            <label className="custom-control-label" htmlFor="customCheck1">
+              Remember me
+            </label>
+          </div>
+        </div>
 
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
-                </div>
+        <button
+          className="btn btn-primary btn-block"
+          onClick={() => this.login(this.state)}
+        >
+          Submit
+        </button>
 
-                <button className="btn btn-primary btn-block" onClick={() => this.login(this.state)}>Submit</button>
-
-                <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
-                </p>
-            </div>
-        );
-    }
+        <p className="forgot-password text-right">
+          Forgot <a href="#">password?</a>
+        </p>
+      </div>
+    );
+  }
 }
