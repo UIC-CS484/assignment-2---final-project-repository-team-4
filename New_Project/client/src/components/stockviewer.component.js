802d77d5196d21d4f35a6b482d851251aec8cdf6
Sean Kim <seank4@uic.edu>
2021-11-09 18:00:45 -0600

    Add Responsive graph

--- New_Project/client/src/components/stockviewer.component.js
+++ New_Project/client/src/components/stockviewer.component.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
+import MyResponsiveLine from "./MyResponsiveLine";
+import data from "../data/mockData.json";
 
 const StockViewer = () => {
   return (
-    <div>
+    <div style={{ height: "400px", width: "800px" }}>
       <h3>Stock Viewer</h3>
+      <MyResponsiveLine data={data}></MyResponsiveLine>
     </div>
   );
 };
