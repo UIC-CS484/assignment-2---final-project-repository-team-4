b48887660c364a14ab32a3f47d2f684f6d4b98c9
kaowayne8 <wkao9@uic.edu>
2021-12-03 14:26:26 -0600

    get rid of cors perms

--- server/index.js
+++ server/index.js
@@ -26,16 +26,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
+  cors()
 );
 app.use(
   session({
