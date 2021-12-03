08c336d557f0c8bb6b5d4f074b96608e4db9337a
kaowayne8 <wkao9@uic.edu>
2021-12-03 14:24:43 -0600

    adde dmore header

--- server/index.js
+++ server/index.js
@@ -17,7 +17,10 @@ const whitelist = [
   "http://tidalstocks.herokuapp.com"
 ];
 app.use((req, res, next) => {
-  res.header({ "Access-Control-Allow-Origin": true });
+  res.header({ "Access-Control-Allow-Origin": "*" });
+  res.header({ "Access-Control-Allow-Credentials": true });
+  res.header({ "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,OPTIONS" });
+  res.header({ "Access-Control-Allow-Origin": "*" });
   next();
 });
 app.use(bodyParser.json());
